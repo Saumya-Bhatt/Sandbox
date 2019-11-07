0fc2a1cb8955c7a80a1a498e0a0aa6f203903e15
Saumya Bhatt <46018242+HighSeinberg@users.noreply.github.com>
2019-11-07 14:28:55 +0530

    php files added

---
+++ php/faq.php
@@ -0,0 +1,48 @@
+
+
+<?php
+$statusMsg = '';
+$msgClass = '';
+if(isset($_POST['submit'])){
+    // Get the submitted form data
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+    
+    // Check whether submitted data is not empty
+    if(!empty($email) && !empty($name) && !empty($subject) && !empty($message)){
+        
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            $statusMsg = 'Please enter your valid email.';
+            $msgClass = 'errordiv';
+        }else{
+            // Recipient email
+            $toEmail = 'sandbox.bpgc@gmail.com';
+            $emailSubject = 'Contact Request Submitted from Sandbox website by '.$name;
+            $htmlContent = '<h2>Contact Request Submitted</h2>
+                <h4>Name</h4><p>'.$name.'</p>
+                <h4>Email</h4><p>'.$email.'</p>
+                <h4>Message</h4><p>'.$message.'</p>';
+
+            // Set content-type header for sending HTML email
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            
+            // Additional headers
+            $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
+            
+            // Send email
+            if(mail($toEmail,$emailSubject,$htmlContent,$headers)){
+                $statusMsg = 'Your contact request has been submitted successfully !';
+                $msgClass = 'succdiv';
+            }else{
+                $statusMsg = 'Your contact request submission failed, please try again.';
+                $msgClass = 'errordiv';
+            }
+        }
+    }else{
+        $statusMsg = 'Please fill all the fields.';
+        $msgClass = 'errordiv';
+    }
+}
+?>
